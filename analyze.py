ae0861f69d28b395f771132a23c4b9df680ef056
William <zizou.ee99@g2.nctu.edu.tw>
2021-09-05 08:58:41 +0800

    first commit

--- analyze.py
+++ analyze.py
@@ -4,6 +4,15 @@ with open('reviews.txt', 'r') as f:
 		data.append(line.strip())
 
 length = len(data)
+length_0 = len(data[0])
 print(length)
-print('data[0] = ', data[0])
-print('data[',length - 1,'] = ', data[length - 1])
+#print('data[0] = ', data[0])
+#print('data[',length - 1,'] = ', data[length - 1])
+#print(length_0)
+cnt = 0
+sum_data_length = 0
+for i in data:
+	length_temp = len(data[cnt])
+	sum_data_length += length_temp
+	cnt += 1 
+print(sum_data_length/length)
